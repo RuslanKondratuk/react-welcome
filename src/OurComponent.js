355c546d074ac22b5d540a8a0e02c7fb1f5271c2
ruslan <ruslan.kondratuk46@gmail.com>
2022-11-09 22:51:00 +0200

    UserCard from react

--- src/OurComponent.js
+++ src/OurComponent.js
@@ -12,16 +12,16 @@ import React from "react";
 //   }
 
 
-class OurComponent extends React.Component{
-    render () {
-        return (
-            <article>
-                <h1>React day is Today</h1>
-                <p>Paragraph</p>
-                <img src = "https://img3.akspic.ru/previews/9/6/1/9/6/169169/169169-ty_zasluzhivaesh_vsyacheskogo_schastya-schaste-strah-voda-polety_na_vozdushnom_share-500x.jpg" />
-            </article>
-        )
-    }
-}
+// class OurComponent extends React.Component{
+//     render () {
+//         return (
+//             <article>
+//                 <h1>React day is Today</h1>
+//                 <p>Paragraph</p>
+//                 <img src = "https://img3.akspic.ru/previews/9/6/1/9/6/169169/169169-ty_zasluzhivaesh_vsyacheskogo_schastya-schaste-strah-voda-polety_na_vozdushnom_share-500x.jpg" />
+//             </article>
+//         )
+//     }
+// }
 
-  export default OurComponent;
+//   export default OurComponent;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import OurComponent from './OurComponent';
 
 
+const card = <Card />
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <OurComponent/>
+  card
 );
 
 
+
+
+function Card (props) {
+
+    const userObj = {
+      "id": 4,
+      "name": "Iness",
+      "description": "k4jh23k4jh23kjhk2jhk",
+      "profilePicture": "https://images.unsplash.com/photo-1573496359142-b8d87734a5a2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=388&q=80"
+    }
+
+
+    return (
+      <section onClick = {() => {console.log('Hello')}}>
+          <img src={userObj.profilePicture} alt = '#'/>
+          <h2>{userObj.name} {userObj.id}</h2>
+          <p>{userObj.description}</p>
+      </section>
+    )
+}
