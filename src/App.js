98db2263df0d8f18a189fd010d36c1a5d8b448d0
ruslan <ruslan.kondratuk46@gmail.com>
2022-11-23 11:57:31 +0200

    add react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,17 +1,39 @@
 import React from "react";
-// import UserDashboard from './components/UserDashBoard'
-// import ListDashBoard from "./components/List DashBoard";
-// import UserCard from "./components/UserCard";
-import WindowSize from "./components/WindowSize";
+import {BrowserRouter, Routes, Route, Link} from 'react-router-dom';
+import Counter from './components/Counter';
+import LoginForm from './components/LoginForm';
+import ToDoList from './components/ToDoList';
+
+
 
 class App extends React.Component {
 
 
     render () {
         return (
-            <WindowSize/>
+            <button onClick={() => {
+                window.close();
+            }}>close</button>
+            // <BrowserRouter>
+            //     <ul>
+            //         <li><Link to='/'>Go to Home</Link></li>
+            //         <li><Link to='/counter'>Go to Counter</Link></li>
+            //         <li><Link to='/login'>Go to LoginForm</Link></li>
+            //         <li><Link to='/todo'>Go to ToDoList</Link></li>
+            //     </ul>
+            //     <Routes>
+            //         <Route path="/" element={<Home/>}/>
+            //         <Route path="/counter" element={<Counter/>}/>
+            //         <Route path="/login" element={<LoginForm/>}/>
+            //         <Route path="/todo" element={<ToDoList/>}/>
+            //     </Routes>
+            // </BrowserRouter>
         )
     }
 }
 
 export default App
+
+const Home = () => {
+    return <h1>You are Home!</h1>
+}
